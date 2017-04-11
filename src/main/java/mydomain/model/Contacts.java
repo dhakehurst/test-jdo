a67ac35e77712ab18e1d35016fca7bc32038834e
David Akehurst <david.akehurst@itemis.de>
2017-04-11 09:40:52 +0200

    added RelatedObjectNotPersisted test

--- src/main/java/mydomain/model/Contacts.java
+++ src/main/java/mydomain/model/Contacts.java
@@ -4,25 +4,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
 @PersistenceCapable(detachable = "true")
 public class Contacts {
-	@PrimaryKey
-	Long id;
 
-	List<Person> list;
+	private Long id;
+
+	private List<Person> list1;
+	private List<Person> list2;
 
 	public Contacts(final long id) {
 		this.id = id;
-		this.list = new ArrayList<>();
+		this.list1 = new ArrayList<>();
+		this.list2 = new ArrayList<>();
 	}
 
+	@PrimaryKey
+	@Persistent
 	public Long getId() {
 		return this.id;
 	}
 
-	public List<Person> getList() {
-		return this.list;
+	public void setId(final Long value) {
+		this.id = value;
+	}
+
+	@Persistent
+	public List<Person> getList1() {
+		return this.list1;
+	}
+
+	public void setList1(final List<Person> value) {
+		this.list1 = value;
+	}
+
+	@Persistent(defaultFetchGroup = "true")
+	public List<Person> getList2() {
+		return this.list2;
+	}
+
+	public void setList2(final List<Person> value) {
+		this.list2 = value;
 	}
 }
--- src/test/java/org/datanucleus/test/SimpleTest.java
+++ src/test/java/org/datanucleus/test/SimpleTest.java
@@ -33,26 +33,30 @@ public class SimpleTest {
 
 	}
 
+	private PersistenceManagerFactory connectDb(final String connectionUrl) {
+		final Map<String, Object> props = new HashMap<>();
+		props.put("javax.jdo.PersistenceManagerFactoryClass", "org.datanucleus.api.jdo.JDOPersistenceManagerFactory");
+		props.put("javax.jdo.option.ConnectionURL", connectionUrl);
+		props.put("datanucleus.schema.autoCreateAll", "true");
+		final PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory(props);
+		return pmf;
+	}
+
+	private void disconnectDb(final PersistenceManagerFactory pmf) {
+		pmf.close();
+	}
+
 	@Test
-	public void simpleTest() {
+	public void LazyLoadQueryResultExceptionIfResultIsEmpty() {
 		NucleusLogger.GENERAL.info(">> test START");
 
-		final Path path = Paths.get("db/neo4j");
+		final String connectionUrl = "neo4j:db/LazyLoadQueryResultExceptionIfResultIsEmpty";
+		final Path path = Paths.get("db/LazyLoadQueryResultExceptionIfResultIsEmpty");
 		if (path.toFile().exists()) {
 			this.delete(path.toFile());
 		}
 
-		final String connectionDriver = "org.neo4j.jdbc.Driver";
-		final String connectionUrl = "neo4j:db/neo4j";
-
-		final Map<String, Object> props = new HashMap<>();
-		props.put("javax.jdo.PersistenceManagerFactoryClass", "org.datanucleus.api.jdo.JDOPersistenceManagerFactory");
-		props.put("javax.jdo.option.ConnectionDriverName", connectionDriver);
-		props.put("javax.jdo.option.ConnectionURL", connectionUrl);
-		props.put("datanucleus.schema.autoCreateAll", "true");
-
-		final PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory(props);
-
+		final PersistenceManagerFactory pmf = this.connectDb(connectionUrl);
 		final PersistenceManager pm = pmf.getPersistenceManager();
 		final Transaction tx = pm.currentTransaction();
 		try {
@@ -85,6 +89,93 @@ public class SimpleTest {
 		}
 
 		pmf.close();
+		NucleusLogger.GENERAL.info(">> test END");
+	}
+
+	@Test
+	public void RelatedObjectNotPersisted() {
+		NucleusLogger.GENERAL.info(">> test START");
+
+		final String connectionUrl = "neo4j:db/RelatedObjectNotPersisted";
+		final Path path = Paths.get("db/RelatedObjectNotPersisted");
+		if (path.toFile().exists()) {
+			this.delete(path.toFile());
+		}
+
+		try {
+			// Create container
+			{
+				final PersistenceManagerFactory pmf = this.connectDb(connectionUrl);
+				final PersistenceManager pm = pmf.getPersistenceManager();
+				final Transaction tx = pm.currentTransaction();
+
+				try {
+					tx.begin();
+					final Contacts c1 = new Contacts(1);
+					pm.makePersistent(c1);
+					tx.commit();
+				} finally {
+					if (tx.isActive()) {
+						tx.rollback();
+					}
+					this.disconnectDb(pmf);
+				}
+
+			}
+
+			// create contained item
+			{
+				final PersistenceManagerFactory pmf = this.connectDb(connectionUrl);
+				final PersistenceManager pm = pmf.getPersistenceManager();
+				final Transaction tx = pm.currentTransaction();
+
+				try {
+					tx.begin();
+					final Query<Contacts> q = pm.newQuery(Contacts.class);
+					final List<Contacts> l = q.executeList();
+					final Contacts c1 = l.get(0);
+					final Person p = new Person(0, "Adam Ant");
+					pm.makePersistent(p);
+					c1.getList1().add(p);
+					c1.getList2().add(p);
+					tx.commit();
+				} finally {
+					if (tx.isActive()) {
+						tx.rollback();
+					}
+					this.disconnectDb(pmf);
+				}
+
+			}
+
+			// check for containment relationship
+			{
+				final PersistenceManagerFactory pmf = this.connectDb(connectionUrl);
+				final PersistenceManager pm = pmf.getPersistenceManager();
+				final Transaction tx = pm.currentTransaction();
+
+				try {
+					tx.begin();
+					final Query<Contacts> q = pm.newQuery(Contacts.class);
+					final List<Contacts> l = q.executeList();
+					final Contacts c1 = l.get(0);
+					Assert.assertEquals(1, c1.getList1().size());
+					Assert.assertEquals(1, c1.getList2().size());
+					tx.commit();
+				} finally {
+					if (tx.isActive()) {
+						tx.rollback();
+					}
+					this.disconnectDb(pmf);
+				}
+
+			}
+
+		} catch (final Throwable thr) {
+			NucleusLogger.GENERAL.error(">> Exception in test", thr);
+			Assert.fail("Failed test : " + thr.getMessage());
+		}
+
 		NucleusLogger.GENERAL.info(">> test END");
 	}
 
